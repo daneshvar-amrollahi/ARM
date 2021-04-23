bbfe522c4efaaff5cad4180b3eeb04dd19629973
daneshvar-amrollahi <amrollahi.daneshvar@gmail.com>
2021-04-24 02:03:29 +0430

    fixed incrementer.v

--- incrementer.v
+++ incrementer.v
@@ -2,5 +2,5 @@ module #(parameter WORD_LEN=32) incrementer(in, out);
     input [WORD_LEN - 1:0] in;
     output [WORD_LEN - 1:0] out;
 
-    assign out = in + 1;
+    assign out = in + 4;
 endmodule
