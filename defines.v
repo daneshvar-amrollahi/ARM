4f8e2f40f97fc5f6ed1bf09a9a6ed7636769b8ad
daneshvar-amrollahi <amrollahi.daneshvar@gmail.com>
2021-04-24 19:25:37 +0430

    added constants for CU and cond_check

--- defines.v
+++ defines.v
@@ -28,3 +28,20 @@
 `define BRANCH_TYPE 2'b10
 
 `define OPCODE_LEN 4
+`define CONDITION_LEN 4
+
+`define EQ 4`b0000
+`define NE 4`b0001
+`define CS_HS 4`b0010
+`define CC_LO 4`b0011
+`define MI 4`b0100
+`define PL 4`b0101
+`define VS 4`b0110
+`define VC 4`b0111
+`define HI 4`b1000
+`define LS 4`b1001
+`define GE 4`b1010
+`define LT 4`b1011
+`define GT 4`b1100
+`define LE 4`b1101
+`define AL 4`b1110
