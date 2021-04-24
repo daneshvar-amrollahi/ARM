24084113253cd6a3a6dfd5af37e7c598b0b45528
moyarka <mahyar.karimi2079@gmail.com>
2021-04-25 01:33:21 +0430

    added ControlUnit to ID_Stage

--- ID_Stage.v
+++ ID_Stage.v
@@ -4,7 +4,21 @@ module ID_Stage(
 	input clk,
 	input rst,
 	input[`ADDRESS_LEN - 1: 0] PC_in,
+	input[`INSTRUCTION_LEN - 1:0] Instruction_in,
 	output[`ADDRESS_LEN - 1: 0] PC
 );
 	assign PC = PC_in;
-endmodule 
+
+	// TODO: instantiate ControlUnit
+	// TODO: instantiate Condition_Check
+
+	wire [`EXECUTE_COMMAND_LEN - 1:0] execute_command;
+	wire mem_read, mem_write, wb_enable, branch_taken, status_write_enable;
+
+	ControlUnit control_unit(.mode(Instruction_in[27:26]),
+			.opcode(Instruction_in[24:21]), .s(Instruction_in[20]),
+			.exe_cmd(execute_command), .mem_read(mem_read), .mem_write(mem_write),
+			.wb_enable(wb_enable), .branch_taken(branch_taken),
+			.status_write_enable(status_write_enable));
+
+endmodule
