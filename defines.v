222baa28900feb3a6fe7e771ada7137ffa02c646
daneshvar-amrollahi <amrollahi.daneshvar@gmail.com>
2021-04-24 17:39:23 +0430

    added register file related constants

--- defines.v
+++ defines.v
@@ -1,3 +1,6 @@
 `define ADDRESS_LEN             32
 `define INSTRUCTION_LEN         32
 `define INSTRUCTION_MEM_SIZE    256 
+`define REGFILE_ADDRESS_LEN     4
+`define REGISTER_LEN            32
+`define REGISTER_MEM_SIZE       15
