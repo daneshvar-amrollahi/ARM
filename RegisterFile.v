48f0dc8a0f3e262888a2b25e8ffd93c272f030fc
daneshvar-amrollahi <amrollahi.daneshvar@gmail.com>
2021-04-24 18:11:05 +0430

    added RegisterFile.v

---
+++ RegisterFile.v
@@ -0,0 +1,24 @@
+`include "defines.v"
+
+module RegisterFile (
+	input clk, rst, 
+    input [`REG_ADDRESS_LEN - 1 : 0] src1, src2, Dest_wb,
+	input[`REGISTER_LEN - 1:0] Result_wb,
+    input writeBackEn,
+	output [`REGISTER_LEN - 1:0] reg1, reg2
+);
+    integer i = 0;
+    reg[`REGISTER_LEN - 1:0] data[0:`REGISTER_MEM_SIZE - 1];
+
+    assign reg1 = data[src1];
+    assign reg2 = data[src2];
+
+    always @(negedge clk, posedge rst) begin
+		if (rst) begin
+            for (i = 0 ; i < `REGISTER_MEM_SIZE ; i = i + 1)
+                data[i] <= i;
+        end
+        else if (writeBackEn) data[Dest_wb] <= Result_wb;
+	end
+    
+endmodule 
