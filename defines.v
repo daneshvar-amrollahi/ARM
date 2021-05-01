e97a92a7df8054ac7c4a07f3138b3ed20cef46f8
moyarka <mahyar.karimi2079@gmail.com>
2021-05-02 02:05:42 +0430

    changed INSTRCUTION_MEM_SIZE in defines.v

--- defines.v
+++ defines.v
@@ -3,7 +3,7 @@
 
 `define ADDRESS_LEN             32
 `define INSTRUCTION_LEN         32
-`define INSTRUCTION_MEM_SIZE    256
+`define INSTRUCTION_MEM_SIZE    2048
 
 `define REGFILE_ADDRESS_LEN     4
 `define REGISTER_LEN            32
