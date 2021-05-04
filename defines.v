d236956f5df6ad1c528434f150eed501ef29036a
daneshvar-amrollahi <amrollahi.daneshvar@gmail.com>
2021-05-04 20:25:04 +0430

    removed opcodes from defines.v

--- defines.v
+++ defines.v
@@ -23,19 +23,7 @@
 
 `define SHIFT_OPERAND_INDEX     11
 `define SHIFT_OPERAND_LEN       12
-`define MOV_EXE  4'b0001
-`define MOVN_EXE 4'b1001
-`define ADD_EXE  4'b0010
-`define ADC_EXE  4'b0011
-`define SUB_EXE  4'b0100
-`define SBC_EXE  4'b0101
-`define AND_EXE  4'b0110
-`define ORR_EXE  4'b0111
-`define EOR_EXE  4'b1000
-`define CMP_EXE  4'b0100
-`define TST_EXE  4'b0110
-`define LDR_EXE  4'b0010
-`define STR_EXE  4'b0010
+
 
 `define S_LDR 1'b1
 `define S_STR 1'b0
