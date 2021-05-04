7568b46b8febca405f2e41bbf7df21666a44fb4e
daneshvar-amrollahi <amrollahi.daneshvar@gmail.com>
2021-05-04 22:58:51 +0430

    fixed some bugs in val2gen

--- val2gen.v
+++ val2gen.v
@@ -12,11 +12,12 @@ module val2gen(
     reg [2 * (`REGISTER_LEN) - 1 : 0] tmp;
     always @(val_rm, shift_operand, immediate, is_mem_command) begin
         integer i;
-        val2_out = {20'b0, shift_operand};
+        val2_out = `REGISTER_LEN'b0;
         tmp = 0;
         if (is_mem_command == 1'b0) begin
 
             if (immediate == 1'b1) begin
+                val2_out = {24'b0 ,shift_operand[7 : 0]};
                 /*
                 for (i = 0; i < shift_operand[11:8] ; i = i + 1) begin
                     val2_out = {val2_out[1], val2_out[0], val2_out[31:2]}; 
@@ -31,22 +32,25 @@ module val2gen(
             if(immediate == 1'b0 && shift_operand[4] == 0) begin
                 case(shift_operand[6:5])
                     `LSL_SHIFT_STATE : begin
-                        val2_out = val2_out << shift_operand[11 : 7];
+                        val2_out = val_rm << shift_operand[11 : 7];
                     end
                     `LSR_SHIFT_STATE : begin
-                        val2_out = val2_out >> shift_operand[11 : 7];
+                        val2_out = val_rm >> shift_operand[11 : 7];
                     end
                     `ASR_SHIFT_STATE : begin
-                        val2_out = val2_out >>> shift_operand[11 : 7];
+                        val2_out = val_rm >>> shift_operand[11 : 7];
                     end
                     `ROR_SHIFT_STATE : begin
-                        tmp = {val2_out, val2_out} >> (shift_operand[11 : 7]);
+                        tmp = {val_rm, val_rm} >> (shift_operand[11 : 7]);
                         val2_out = tmp[`REGISTER_LEN - 1 : 0];
                     end
                 endcase
             end
         end 
-
+        else //is mem_command
+        begin
+           val2_out = {24'b0, shift_operand[11 : 0]}; 
+        end
 
     end
 endmodule 
