a9d97ab956c3e35f784a2e2bd0c62a18569a2bab
daneshvar-amrollahi <amrollahi.daneshvar@gmail.com>
2021-05-07 01:07:36 +0430

    added new instructions

--- memory.v
+++ memory.v
@@ -21,9 +21,8 @@ module memory(clk, rst, addr, write_data, mem_read, mem_write, read_data);
             {data[8], data[9], data[10], data[11]} <= `INSTRUCTION_LEN'b1110_00_1_1101_0_0000_0010_000100000011;   //   MOV R2 ,#0xC0000000    R2 = -1073741824
             {data[12], data[13], data[14], data[15]} <= `INSTRUCTION_LEN'b1110_00_0_0100_1_0010_0011_000000000010; //   ADDS R3 ,R2,R2         R3 = -2147483648 
             {data[16], data[17], data[18], data[19]} <= `INSTRUCTION_LEN'b1110_00_0_0101_0_0000_0100_000000000000; //ADC R4 ,R0,R0 //R4 = 41
-
-            //{data[20], data[21], data[22], data[23]} <= `INSTRUCTION_LEN'b1110_01_0_0100_0_0000_0010_000000000100;  //STR R2 ,[R0],#4 //MEM[1028] = -1073741824; 
-            //{data[24], data[25], data[26], data[27]} <= `INSTRUCTION_LEN'b0; //For haj Mahyar!
+            {data[20], data[21], data[22], data[23]} <= `INSTRUCTION_LEN'b1110_00_0_0010_0_0100_0101_000100000100; //SUB R5 ,R4,R4,LSL #2 //R5 = -123 
+            {data[24], data[25], data[26], data[27]} <= `INSTRUCTION_LEN'b0; 
         end
         
         else if (mem_write)
