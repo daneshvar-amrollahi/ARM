65320cc4e5d51fb5b2eaa051948f5156d33a035e
daneshvar-amrollahi <amrollahi.daneshvar@gmail.com>
2021-05-07 02:40:42 +0430

    fixed mux2to1

--- mux2to1.v
+++ mux2to1.v
@@ -1,7 +1,15 @@
 module mux2to1 #(parameter WORD_LEN=32) (a, b, sel_a, sel_b, out);
     input [WORD_LEN - 1:0] a, b;
     input sel_a, sel_b;
-    output [WORD_LEN - 1:0] out;
+    output reg [WORD_LEN - 1:0] out;
 
-    assign out = (sel_a == 1'b1) ? a : ( (sel_b == 1'b1) ? b : out);
+    //assign out = (sel_a == 1'b1) ? a : ( (sel_b == 1'b1) ? b : out);
+    always @(*)
+    begin
+        out = 0;
+        if (sel_a)
+            out = a;
+        else if (sel_b)
+            out = b; 
+    end
 endmodule
